ac049d2d9085c906e492c7de0f94bb0e6878e149
LENOVO <LENOVO@Arjun>
2022-04-12 15:56:34 +0530

    string practice class

---
+++ Collection Frame/src/Strings/Practice.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" This calss is for interview practise");
+
+	}
+
+}
